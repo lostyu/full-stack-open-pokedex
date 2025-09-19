015cc712b69fa5bf6aea9998cfe0c65b51b3f518
lostyu <lostyu789@163.com>
2025-09-19 12:34:28 +0800

    modify vercel node server

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+// app.listen(PORT, () => {
+//   console.log(`server started on port ${PORT}`)
+// })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
+
+module.exports = app
